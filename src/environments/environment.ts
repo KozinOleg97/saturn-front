ae9bdedf3641257784e47e7013903246f1c6b1bf
KozinOleg97 <olegkosin.dev@yande.ru>
2023-02-22 12:29:22 +0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import {EnvironmentInterface} from "./environment-interface";
 
 export const environment: EnvironmentInterface = {
   production: false,
-  apiUrl: 'http://0.0.0.0:8080/api/v1',
+  apiUrl: 'http://127.0.0.1:8080/api/v1',
 };
